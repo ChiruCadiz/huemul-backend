07fa29ab9ad9001128be83f479bb7fff259c110b
ChiruCadiz <cadiz.ch@gmail.com>
2026-06-08 19:43:47 -0400

    nueva tabla sesiones

--- app/db/models.py
+++ app/db/models.py
@@ -1,4 +1,9 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime, func
+import uuid
+from sqlalchemy import (
+    Column, Integer, String, Boolean,
+    Text, DateTime, ForeignKey, func
+)
+from sqlalchemy.dialects.postgresql import UUID
 from app.db.database import Base
 
 class User(Base):
@@ -16,3 +21,14 @@ class Config(Base):
     id    = Column(Integer, primary_key=True)
     key   = Column(String(100), unique=True, nullable=False)
     value = Column(Text, nullable=True)
+
+class Session(Base):
+    __tablename__ = "sessions"
+
+    id             = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_id        = Column(Integer, ForeignKey("users.id"), nullable=False)
+    title          = Column(String(255), nullable=True)
+    model_used     = Column(String(100), nullable=False, default="codellama")
+    is_active      = Column(Boolean, default=True, nullable=False)
+    created_at     = Column(DateTime, server_default=func.now())
+    last_active_at = Column(DateTime, server_default=func.now())
